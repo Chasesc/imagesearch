579550b7507625879f1db121afe706b565cd3113
Chase Schelthoff <schelthoff.chase@gmail.com>
2019-04-29 21:45:51 -0400

    add predict method to ImageInterface

--- imagesearch/image_inference.py
+++ imagesearch/image_inference.py
@@ -2,12 +2,23 @@
 Handles ML inference of images. (extract features, class, etc)
 '''
 
+import numpy as np
+
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras import applications
 
 models = {
     'mobilenet' : dict(module=applications.mobilenet,
                        model='MobileNet',
                        features_layer='global_average_pooling2d',
+                       shape=(224, 224, 3),
+                       args=(),
+                       kwargs=dict()),
+
+    'xception' : dict(module=applications.xception,
+                       model='Xception',
+                       features_layer='global_average_pooling2d',
+                       shape=(299, 299, 3),
                        args=(),
                        kwargs=dict())
 }
@@ -24,9 +35,20 @@ class ImageInference(object):
         kwargs = model_info['kwargs']
 
         self.model_module = model_info['module']
+        self.image_shape  = model_info['shape']
 
         self.decode_predictions = getattr(self.model_module, 'decode_predictions')
         self.preprocess_input   = getattr(self.model_module, 'preprocess_input')
         self.model              = getattr(self.model_module, model_info['model'])(*args, **kwargs)
 
         if not quiet: self.model.summary()
+
+    def predict(self, img):
+        # apply transforms
+        img = image.load_img(img, target_size=self.image_shape)
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = self.preprocess_input(img)
+
+        preds = self.decode_predictions(self.model.predict(img), top=3)[0]
+        return preds
--- imagesearch/image_search.py
+++ imagesearch/image_search.py
@@ -7,18 +7,18 @@ from pathlib import Path
 from skimage.io import imread
 
 def load_image_from_path(path):
-    return imread(path)[:, :, :3] # just keep rgb
+    return imread(path, plugin='matplotlib')[:, :, :3] # just keep rgb
 
 class ImageSearch(object):
     def __init__(self):
-        self.inference = image_inference.ImageInference()
+        self._inference = image_inference.ImageInference()
         self._images = {}
 
     def add_images(self, *images):
         for image in images:
             image = str(image) # ensure we have a string and not a pathlib path
-            self._images[image] = load_image_from_path(image)
-            print(self._images[image].shape, image)
+
+            self._images[image] = self._inference.predict(image)
     
     def add_directories(self, *directories, filetypes=('jpg', 'jpeg', 'png'), recursive=False):
         images = []
