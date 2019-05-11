d3fe18ca7633a676a91c312e73e928fea3fd9234
Chase Schelthoff <schelthoff.chase@gmail.com>
2019-05-11 17:01:11 -0400

    added basic similarity detection based on image features

--- imagesearch/image_search.py
+++ imagesearch/image_search.py
@@ -1,18 +1,27 @@
 '''
 '''
-
 from pathlib import Path
 from skimage.io import imread
+import numpy as np
 
 import image_inference
 import image_utils
 
 class ImageSearch(object):
     def __init__(self, cache_images=True):
-        self._inference = image_inference.ImageInference()
-        self._images = {}
+        self.images = {}
 
+        self._inference = image_inference.ImageInference()
         self._cache_images = cache_images
+
+    def find_similar(self, img_path, method='features'):
+        if img_path not in self.images:
+            self.add_images(img_path)
+
+        if method == 'features':
+            return self._find_similar_features(img_path)
+        else:
+            raise ValueError(f'Method "{method}" not yet supported')        
 
     def add_images(self, *images):
         for image in images:
@@ -27,7 +36,7 @@ class ImageSearch(object):
                 'image'       : image_utils.pil_to_array(pil_image) if self._cache_images else None
             }
 
-            self._images[image] = image_info
+            self.images[image] = image_info
 
     def add_directories(self, *directories, filetypes=('jpg', 'jpeg', 'png'), recursive=False):
         images = []
@@ -41,10 +50,23 @@ class ImageSearch(object):
 
         self.add_images(*images)
 
+    def _find_similar_features(self, img_path):
+        distances = []
+        for path in self.images:
+            if path != img_path:
+                dist = self._feature_distance(img_path, path)
+                distances.append((dist, path))
+
+        return sorted(distances, key=lambda t:t[0])
+
+    def _feature_distance(self, first_img_path, second_img_path):
+        first_img_features  = self.images[first_img_path]['features']
+        second_img_features = self.images[second_img_path]['features']
+        return np.linalg.norm(second_img_features - first_img_features)
+
 def test():
     imgsearch = ImageSearch()
-    imgsearch.add_directories('/home/chases/Pictures/cats')
-    a = 1 # for a breakpoint
+    imgsearch.add_directories('/home/chases/Pictures/', recursive=True)
 
 if __name__ == '__main__':
     test()
